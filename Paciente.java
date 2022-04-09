71f02f1a2d9623dab05d074ddfa214c311fee465
iguerrero <vermasalla@yahoo.com.ar>
2022-04-09 15:40:48 -0300

    Clase Paciente, actualización

--- Paciente.java
+++ Paciente.java
@@ -1,10 +1,30 @@
+import java.time.ZoneId;
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class Paciente {
 
     private int id;
     private String nombre;
     private String apellido;
+    private String domicilio;
     private int dni;
     private String clave;
+    private Date fechaNac;
+
+    public void paciente() {
+
+    }
+
+    public void paciente(String nombre, String apellido, String domicilio, int dni, Date fechaNac) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.dni = dni;
+        this.fechaNac = fechaNac;
+    }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -20,5 +40,30 @@ public class Paciente {
 
     public String getApellido() {
         return apellido;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public int getEdad() {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate nacimiento = fechaNac.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate ahora = LocalDate.now();
+
+        Period periodo = Period.between(nacimiento, ahora);
+        return periodo.getYears();
     }
 }
